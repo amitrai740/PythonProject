fe71f003e249e5b968a225a7ea94741efc677f6c
Rohit Kumar <rohit.kr.0310@gmail.com>
2022-10-11 22:40:57 +0200

    hello world program

--- LCM.py
+++ LCM.py
@@ -1,6 +1,6 @@
 # find Lcm of two number
-a=int(input("enter 1st number"))
-b=int(input("enter 2nd number"))
+a=int(input("enter 1st number : "))
+b=int(input("enter 2nd number : "))
 m=max(a,b)
 for i in range(m,a*b+1,m):
     if i%a==0 and i%b==0:

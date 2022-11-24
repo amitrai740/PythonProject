f76ed985cf83bf206b00c667442d8f4cf9fa749a
Amit Kumar <amitkumarrai740@gmail.com>
2022-11-24 15:33:51 +0530

    Search In rotated list

---
+++ SearchInRotatedSortedList.py
@@ -0,0 +1,19 @@
+lst=[5,6,1,2,3,4]
+target=4
+l=0
+u=len(lst)-1
+while(l<=u):
+    mid=(l+u)//2
+    if(lst[mid]==target):
+        print(mid)
+        exit(0)
+    if(lst[l]<=lst[mid]):
+        if(target>=lst[l] and target<lst[mid]):
+            u=mid-1
+        else:
+            l=mid+1
+    else:
+        if(target>lst[mid] and target<=lst[u]):
+            l=mid+1
+        else:
+            u=mid-1
